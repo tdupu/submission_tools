b471a0f6c7d6b9567123b99248b6102359324e91
Taylor Dupuy <taylor.dupuy@gmail.com>
2020-08-10 05:21:04 -0400

    adding nosetests

---
+++ submissions_nosetests.py
@@ -0,0 +1,16 @@
+path_to_data = "../../data/algebra-one/20/f/"
+
+def test_authenticate1():
+    
+    assert re.search(key,"copper")
+    
+def test_authenticate2():
+    
+    assert re.search(key,"copper")
+    
+def is_valid_assignment():
+
+def test_submit_problem():
+    
+def test_is_valid_review():
+
