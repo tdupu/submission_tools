90ecb56ebcb38ab090d9909e226e3129f83d3e1f
Taylor Dupuy <taylor.dupuy@gmail.com>
2020-10-02 03:57:26 -0400

    corrected a bug that failed when computing standard deviations

--- dashboard.py
+++ dashboard.py
@@ -236,7 +236,10 @@ for p0 in exercises:
     if len(total_scored2)>0:
         mean0=mean(total_scored2)
         med0=median(total_scored2)
-        std0=stdev(total_scored2)
+        if total_scored2>1:
+            std0=stdev(total_scored2)
+        else:
+            std0=0
         time_data=get_times(scored2)
         rt0=mean(time_data['review_times'])
         mt0=mean(time_data['matching_times'])
